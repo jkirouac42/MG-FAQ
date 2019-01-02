90afb24d7487694de36a5814d67838ad4221966d
Jason Kirouac <jkirouac42@gmail.com>
2019-01-02 08:53:42 -0500

    backround gradient and hover

---
+++ faq.js
@@ -0,0 +1,7 @@
+document.getElementById('oct').addEventListener("click", octoprint, false);
+
+function octoprint{
+    if(document.querySelectorAll('*[id]')!= "oct2" || "oct1"){
+        document.document.querySelectorAll('*[id]')style.display = "none";
+        }
+}
