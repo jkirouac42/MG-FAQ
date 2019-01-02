1d6f49fd7bb6788c78482efb20c405af34e34570
Jason Kirouac <jkirouac42@gmail.com>
2019-01-02 15:28:52 -0500

    added jquery toggles

--- faq.js
+++ faq.js
@@ -1,9 +1,21 @@
-function addEventListener(){
-    document.getElementById('oct').addEventListener("click", hide1, false);
+
+/*jshint unused:false*/
+/*global $, jQuery, alert*/
+
+function hide(){
+    "use strict";
+    $("#s3d").slideToggle();
+    $("#octoprint").slideToggle();    
 }
 
-function hide1(){
-    console.log('works');
-    document.getElementById('s3d').display = "none";
-    document.getElementById('maint').display = "none";
+function hide1() {
+    "use strict";
+    $("#s3d").slideToggle();
+    $("#maint").slideToggle();
+}
+
+function hide2() {
+    "use strict";
+    $("#octoprint").slideToggle();
+    $("#maint").slideToggle();
 }
